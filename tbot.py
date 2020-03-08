d26dfc6b218a5779badd69f33b0e3c60673a2972
eliastg <eliastrenard@gmail.com>
2020-03-07 23:27:02 -0500

    Simple program for the designed output.
    Just a simple output, similar to the Use Case definition.

---
+++ tbot.py
@@ -0,0 +1,33 @@
+import sys
+import datetime
+#---------------------------------
+def printInitializationMessage():
+	print('TBOT: (Trading Bot)')
+	print('Starting at: <datetime>')
+	print('Config file: /path/to/config.ini')
+	print('Coin per: <coin-per>')
+	print('Strategy: <straategy>')
+	print('===============')
+	print('tbot Execution BEGIN')
+	print('===============')
+
+def printFinalizationMessage():
+	print('===============')
+	print('tbot Execution END')
+	print('===============')
+
+def userPressCtrC():
+	return False
+
+def getPrice():
+	print("<datetime> <coin-per>: <price>")
+
+#-------Main----------------------
+if __name__ == '__main__':
+	
+	printInitializationMessage()
+
+	while (userPressCtrC()):
+		price = getPrice()
+	
+	printFinalizationMessage()
