36c66e40e5843f11d855094fc42c34ba97268fed
eliastg <eliastrenard@gmail.com>
2020-03-28 23:00:06 -0400

    A simple state transition. It just prints the transition.
    A simple state trnsition is defined for the bot.
    There are 6 states.

--- tradingbot.py
+++ tradingbot.py
@@ -6,6 +6,13 @@ class TBot:
 
 	configuration = None
 
+	stateProcessing = "processing"
+	stateOperating = "operating"
+	stateObserving = "observing"
+	stateClosing = "closing"
+	stateFinal = "final"
+	stateError = "error"
+
 	def start(self):
 		config = setup.Configuration()
 		configResult = config.load()
@@ -15,11 +22,7 @@ class TBot:
 		self.configuration = config
 		print(config.getConfigOutput())
 		
-		while True:
-			self.waitTime()
-			self.queryPrice()
-
-		exit(0)
+		self.trade()
 
 	def waitTime(self):
 		time.sleep(1)
@@ -35,3 +38,47 @@ class TBot:
 			print(response['message'])
 		else:
 			print('BTC_USD: {}'.format(response['result']['last']))
+
+	def trade(self):
+		state = self.stateProcessing
+
+		while state != self.stateFinal:
+
+			print("State: {}".format(state))
+			self.waitTime()
+
+			if state == self.stateProcessing:
+				state = self.actionStateProcessing(state)
+			
+			elif state == self.stateOperating:
+				state = self.actionStateOperating(state)
+
+			elif state == self.stateObserving:
+				state = self.actionStateObserving(state)
+
+			elif state == self.stateClosing:
+				state = self.actionStateClosing(state)
+
+			elif state == self.stateError:
+				state = self.actionStateError(state)
+		
+		self.actionStateFinal()
+
+
+	def actionStateProcessing(self, state):
+		return self.stateOperating
+	
+	def actionStateOperating(self, state):
+		return self.stateObserving
+
+	def actionStateObserving(self, state):
+		return self.stateClosing
+
+	def actionStateClosing(self, state):
+		return self.stateProcessing
+
+	def actionStateError(self, state):
+		return self.stateFinal
+		
+	def actionStateFinal(self):
+		print("Trading activity stopped.")
